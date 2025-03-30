ef13b50ff8852d3400917c3c538748fb4811209f
N3XAN <160686853+N3X4N@users.noreply.github.com>
2025-03-30 01:06:34 -0500

    first commit

---
+++ archiva/resources/views/auth/login.blade.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Título</title>
+    <!-- Meta etiquetas requeridas -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="{{ asset('assets/login.css') }}">
+</head>
+
+<body>
+
+    <section class="h-100 gradient-form" style="background-color: #eee;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-xl-10">
+                    <div class="card rounded-3 text-black">
+                        <div class="row g-0">
+                            <div class="col-lg-6">
+                                <div class="card-body p-md-5 mx-md-4">
+
+                                    <div class="text-center">
+                                        <img src="{{ asset('assets/archiva.png') }}" style="width: 185px;"
+                                            alt="logo">
+                                        <h4 class="mt-1 mb-5 pb-1">Somos el equipo Lotus</h4>
+                                    </div>
+
+                                    <form action="{{ route('login') }}" method="POST">
+                                        @csrf
+                                        <p>Por favor, inicia sesión en tu cuenta</p>
+
+                                        <div class="form-outline mb-4">
+                                            <label class="form-label" for="email">Usuario</label>
+                                            <input type="email" name="email" id="email"
+                                                class="form-control @error('email') is-invalid @enderror"
+                                                placeholder="correo electrónico" value="{{ old('email') }}" required
+                                                autocomplete="email" autofocus />
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <label class="form-label" for="password">Contraseña</label>
+                                            <input type="password" name="password" id="password"
+                                                class="form-control @error('password') is-invalid @enderror" required
+                                                autocomplete="current-password" />
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-check mb-3">
+                                            <input class="form-check-input" type="checkbox" name="remember"
+                                                id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="remember">
+                                                Recordarme
+                                            </label>
+                                        </div>
+
+                                        <div class="text-center pt-1 mb-5 pb-1">
+                                            <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                                                type="submit">Iniciar sesión</button>
+                                            @if (Route::has('password.request'))
+                                                <a class="text-muted" href="{{ route('password.request') }}">¿Olvidaste
+                                                    tu contraseña?</a>
+                                            @endif
+                                        </div>
+
+                                        <div class="d-flex align-items-center justify-content-center pb-4">
+                                            <p class="mb-0 me-2">¿No tienes una cuenta?</p>
+                                            <a href="{{ route('register') }}" class="btn btn-outline-danger">Crear
+                                                nueva</a>
+                                        </div>
+
+                                    </form>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                    <h4 class="mb-4">Alcaldía de Tabio</h4>
+                                    <p class="small mb-0">El sistema de gestión documental para la Alcaldía de Tábio es
+                                        una solución tecnológica diseñada para optimizar la administración de documentos
+                                        y trámites administrativos. Este sistema permite almacenar fisicamente control , estados y
+                                        gestionar eficientemente documentos oficiales, facilitando su
+                                        acceso y consulta de manera rápida y segura. Además, contribuye a la mejora en
+                                        la transparencia, la trazabilidad de los procesos y la reducción del uso de
+                                        papel, promoviendo la eficiencia y la sostenibilidad dentro de la institución.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Bibliotecas JavaScript de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ archiva/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-admin-layout>
+    <x-slot name="title">
+      Dashboard
+    </x-slot>
+
+    <h1>Bienvenido al Dashboard</h1>
+    <p>Aquí va el contenido principal de tu aplicación.</p>
+  </x-admin-layout>
